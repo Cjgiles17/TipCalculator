c4122b375dcc33d37dda2510027572059334090c
Caleb Giles <calebgiles@calebs-macbook-pro.local>
2019-01-20 14:55:43 -0500

    Initial Commit

---
+++ TipCalculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TipCalculator
+//
+//  Created by Caleb Giles on 1/20/19.
+//  Copyright © 2019 Caleb Giles. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
